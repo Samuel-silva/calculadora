74b608f0b08ac5c2ef372f86daafdf93e2a8560e
Samuel Barbosa <samuca192silva@hotmail.com>
2023-03-30 12:08:33 -0300

    feat - Starting project

---
+++ src/Calculator.jsx
@@ -0,0 +1,12 @@
+import './Calculator.css'
+import React, { Component } from 'react'
+
+export default class Calculator extends Component {
+  render() {
+    return (
+      <div className="content">
+        <div className="calculator">Calculadora</div>
+      </div>
+    )
+  }
+}
