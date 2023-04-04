f9dd13cc645f9b7f7f0ac723b65a615575746545
Samuel Barbosa <samuca192silva@hotmail.com>
2023-04-04 18:40:17 -0300

    feat - Show number on display

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,19 @@ import React from "react"
 import './Button.css'
 
 export default function Button(props) {
-    const { label } = props
-    let classeCss = 'button'
-    classeCss += props.double ? ' double' : ''
-    classeCss += props.triple ? ' triple' : ''
-    classeCss += props.operation ? ' operation' : ''
+  const { label, click = e => e } = props
+  const value = props.value || label
+  let classeCss = 'button'
+  classeCss += props.double ? ' double' : ''
+  classeCss += props.triple ? ' triple' : ''
+  classeCss += props.operation ? ' operation' : ''
 
-    return (
-        <button className={classeCss}>{ label }</button>
-    )
+  return (
+    <button
+      onClick={e => click(value)}
+      className={classeCss}
+    >
+      {label}
+    </button>
+  )
 }
